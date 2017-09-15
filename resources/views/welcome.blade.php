ffa2312180e37674ef5165277752272ae90822e0
edyulisses <edyulisses@gmail.com>
2017-09-14 23:00:31 -0300

    Translate Views Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Bem Vindo</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Sistema de Post de Conteúdo com compartilhamento via QRCode.
                 </div>
             </div>
         </div>
