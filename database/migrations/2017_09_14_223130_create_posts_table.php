07f8305282b31bff0dc658d80de447ea5e8b8ceb
edyulisses <edyulisses@gmail.com>
2017-09-14 22:51:05 -0300

    Create Table Posts Using Migration
    php artisan make:migration create_items_table
    php artisan migrate

---
+++ database/migrations/2017_09_14_223130_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                            ->onUpdate('cascade')->onDelete('cascade');            
+
+            $table->string('titulo');
+            $table->text('descricao');
+            $table->text('texto');
+            $table->string('qrcode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop("posts");
+    }
+}
